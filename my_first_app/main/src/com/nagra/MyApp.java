7bf7b1721e3aa4bd5a66922e413a47876679aec1
Sumanth <sugs19cs@cmrit.ac.in>
2023-04-17 16:06:28 +0530

    i changed again

--- my_first_app/main/src/com/nagra/MyApp.java
+++ my_first_app/main/src/com/nagra/MyApp.java
@@ -2,6 +2,6 @@ package com.nagra;
 
 public class MyApp {
     public static void main(String[] args) {
-        System.out.println("Hello world! This is Sumanth");
+        System.out.println("Hello world! This is Sumanth GS");
     }
 }
